7c25e1977c25d9525f81dbeafd7d968a514ca452
Anna <awoellnitz@web.de>
2025-10-02 13:05:49 +0200

    dark-mode_finished

---
+++ js-basics/js-dom-and-events_simple-calculator/js/index.js
@@ -0,0 +1,77 @@
+console.clear();
+
+const operand1 = 12;
+const operand2 = 4;
+
+// ----- Mathematical Operations -----
+
+// Step 1: Use `document.querySelector` to select each button by its `data-js` attribute.
+
+// --v-- write your code here --v--
+const addButton = document.querySelector("[data-js=add]")
+console.log(addButton)
+const subtractButton = document.querySelector("[data-js=subtract]")
+console.log(subtractButton)
+const multiplyButton = document.querySelector("[data-js=multiply]")
+console.log(multiplyButton)
+const divideButton = document.querySelector("[data-js=divide]")
+console.log(divideButton)
+const exponentButton = document.querySelector("[data-js=exponent]")
+console.log(exponentButton)
+const moduloButton = document.querySelector("[data-js=modulo]")
+console.log(moduloButton)
+// --^-- write your code here --^--
+
+/* 
+Step 2: Add event listeners for each mathematical operation:
+
+For each operation (add, subtract, multiply, divide, exponent, and modulo):
+1. Add an event listener to the corresponding button.
+2. Within the event listener, perform the operation using `operand1` and `operand2`.
+3. Store the result in a variable.
+4. Log the result to the console.
+*/
+
+// --v-- write your code here --v--
+addButton.addEventListener("click", () => {
+    const resultAdd = operand1 + operand2;
+    console.log(resultAdd)
+})
+subtractButton.addEventListener("click", () => {
+    const resultSub = operand1 - operand2;
+    console.log(resultSub)
+})
+multiplyButton.addEventListener("click", () => {
+    const resultMul = operand1 * operand2;
+    console.log(resultMul)
+})
+divideButton.addEventListener("click", () => {
+    const resultDiv = operand1 / operand2;
+    console.log(resultDiv)
+})
+exponentButton.addEventListener("click", () => {
+    const resultExpo = operand1 ** operand2;
+    console.log(resultExpo);
+})
+moduloButton.addEventListener("click", () => {
+    const resultModu = operand1 % operand2;
+    console.log(resultModu);
+})
+// --^-- write your code here --^--
+
+// ----- Update the First Operand -----
+
+/*
+In the following section, update the value of `operand1` using the buttons in the "Update the First Operand" section.
+Each button should adjust the value of `operand1` and log the new value to the console.
+
+Hint: To allow `operand1` to be updated, you might need to change its declaration.
+
+Step 1: Select each button for updating `operand1` by its `data-js` attribute.
+Step 2: Add event listeners to update `operand1` based on the button clicked. Log the updated value to the console.
+*/
+
+// --v-- write your code here --v--
+let operand = 10;
+const operand2 = 4;
+// --^-- write your code here --^--
