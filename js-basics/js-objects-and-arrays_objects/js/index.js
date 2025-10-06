db16336d47e3bb26332d41f1f453fab5cbcbd997
Anna <awoellnitz@web.de>
2025-10-06 10:51:45 +0200

    object_finished

---
+++ js-basics/js-objects-and-arrays_objects/js/index.js
@@ -0,0 +1,60 @@
+console.clear();
+
+// EXERCISE 1
+// Create an object `personWithNameAgeEmail` with the properties "name", "age" and "email".
+
+const personWithNameAgeEmail = {
+  name: "Anna",
+  age: 30,
+  email: "muster.name@gmail.com"
+};
+console.log(personWithNameAgeEmail.age)
+
+
+// EXERCISE 2
+// Set the values of `nameOfAlex` and `ageOfAlex` to match the corresponding values in the `personAlex` object.
+
+const personAlex = {
+  name: "Alex",
+  age: 24,
+};
+
+const nameOfAlex = "Alex";
+const ageOfAlex = "24";
+
+console.log(ageOfAlex)
+
+// EXERCISE 3
+// Change the `name` to "Alex" and `age` to "35" by updating the `personToChange`object: `person.existingProperty = newValue`.
+
+const personToChange = {
+  name: "Kim",
+  age: 24,
+};
+
+personToChange.name="Alex",
+personToChange.age=35,
+
+console.log(personToChange)
+
+
+// EXERCISE 4
+// Add the properties `age` with value "5" and `breed` with value "husky" to the `petPluto` object: `pet.newProperty = newValue`.
+
+const petPluto = {
+  name: "Pluto",
+  species: "dog",
+};
+
+petPluto.age=5,
+petPluto.breed="husky"
+
+console.log(petPluto)
+
+export {
+  personWithNameAgeEmail,
+  nameOfAlex,
+  ageOfAlex,
+  personToChange,
+  petPluto,
+};
