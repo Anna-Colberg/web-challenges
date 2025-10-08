e18e60ed1a1f8a4355a2f1702f5ac8e864648289
Anna <awoellnitz@web.de>
2025-10-08 09:57:53 +0200

    box_finished2

--- js-basics/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-basics/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -7,17 +7,13 @@ const box = document.querySelector('[data-js="box"]')
 
 
 colorslider.addEventListener("input", () => {
-    box.style.backgroundColor = `hsl(${colorslider.value}, 100%, 50%)`;  
-})
+    box.style.backgroundColor = `hsl(${colorslider.value}, 70%, 60%)`;  
+});
 
 radiusslider.addEventListener("input", () => {
-    if (radiusslider.value == radiusslider.max) {
-        box.style.borderRadius = "30%";
-    } else {
-        box.style.borderRadius = `${radiusslider.value}px`;
-    }
-})
+   box.style.borderRadius = `${radiusslider.value}%`;
+});
 
 rotationslider.addEventListener("input", () => {
     box.style.transform = `rotate(${rotationslider.value}deg)`;
-})
+});
