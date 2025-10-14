a20a1aa2b92363edd8d815fc29305692c5b601d7
Anna <awoellnitz@web.de>
2025-10-14 15:49:57 +0200

    arrayforeach_start

---
+++ js-basics/js-array-methods_for-each/js/index.js
@@ -0,0 +1,36 @@
+/* const { createElement } = require("react"); */
+
+const colors = [
+  "#343434",
+  "#7FB5B5",
+  "#633A34",
+  "#354D73",
+  "#F3A505",
+  "#2A6478",
+  "#CC0605",
+  "#497E76",
+  "#D36E70",
+  "#FF2301",
+  "#4C514A",
+  "#2E3A23",
+  "#5D9B9B",
+  "#474A51",
+  "#EFA94A",
+  "#4D5645",
+  "#606E8C",
+  "#A5A5A5",
+];
+
+colors.forEach(function (color) {
+  const div = document.createElement("div");
+  div.classList.add("color-box");
+  console.log(color);
+});
+
+
+
+
+/* 
+1. Für jeden Hexadezimalcode sollte ein neues „div“ mit der Klasse „color-box“ erstellt werden.
+2. Die „background-color“ des „div“ sollte dem aktuellen Hexadezimalcode entsprechen.
+3. Das Element sollte der Seite hinzugefügt werden. */
