4c7c0421e920ff5784f829ea131bb5b9b6807cd3
Anna <awoellnitz@web.de>
2025-10-14 17:15:57 +0200

    arrayfilter_start

---
+++ js-basics/js-array-methods_filter/js/index.js
@@ -0,0 +1,48 @@
+console.clear();
+
+const cards = [
+  {
+    id: "1",
+    isBookmarked: false,
+    question: "How often can I use <header>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "2",
+    isBookmarked: false,
+    question: "How often can I use <aside>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "3",
+    isBookmarked: true,
+    question: "On which types can I use destructuring assignment?",
+    answer: "On Objects and Arrays",
+    tags: ["js", "next"],
+  },
+];
+
+const onlyCardWithIdTwo = cards.filter(function (card) {
+  
+});
+
+const allCardsWith3Tags = cards.filter(function (card) {
+
+});
+
+const allCardsThatAreNotBookmarked = cards.filter(function (card) {
+
+});
+
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(function (card) {
+  
+});
+
+export {
+  onlyCardWithIdTwo,
+  allCardsWith3Tags,
+  allCardsThatAreNotBookmarked,
+  allCardsWithTagsHTMLOrJSThatAreBookmarked,
+};
