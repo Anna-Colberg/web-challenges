e3810cf0aa1859a82a66d7bbc424696effacac4f
Anna <awoellnitz@web.de>
2025-10-16 15:51:02 +0200

    fetch_finished

---
+++ js-basics/js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,16 @@
+
+console.clear();
+
+
+const url = "https://swapi.py4e.com/api/people";
+
+
+
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.results[8])
+}
+
+
+fetchData();
