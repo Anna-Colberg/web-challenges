188be6646dc4da3570d0dcec58f41c73e665da62
Anna <awoellnitz@web.de>
2025-10-24 13:54:53 +0200

    smiley_finished

--- react-basics/react-props_smiley/src/App.jsx
+++ react-basics/react-props_smiley/src/App.jsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy />
+      <Smiley />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return (
+    <span aria-label={isHappy ? "A happy smiley" : "A sad smiley"}>
+      {isHappy ? "😌" : "😭"}
+    </span>
+  );
 }
