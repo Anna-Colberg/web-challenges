17c9a28e0d0666f866f90989364ffbae7360d317
Anna <awoellnitz@web.de>
2025-10-24 08:42:00 +0200

    react-basics_finished

---
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+export default function App() {
+  return <article className="article"><a href="https://de.wikipedia.org/wiki/Haushund"className="article__link">Der Haushund</a>
+    <h2 className="article__title">Article</h2>
+    <label htmlFor="article__test" id="article">Hello</label>
+    <input type="text" id="article" htmlFor/>Der Hund ist der Beste Freund eines Menschen</article>;
+}
---
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export default function HelloWorldArticle() {
+  return <article>
+    <h1>HelloWorldArticle</h1>
+    <p>Hallo, das ist ein Test und er funktioniert</p></article>;
+}
