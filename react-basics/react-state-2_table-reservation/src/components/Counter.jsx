b9f2ef843a098b261ba2985bc6e6a557607e90fc
Anna <awoellnitz@web.de>
2025-10-28 20:55:23 +0100

    finished

---
+++ react-basics/react-state-2_table-reservation/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+/* import { useState } from "react"; */
+
+export default function Counter({people, onSetPeople}) {
+  return (
+    <>
+      <h2>How many people would you like to visit us with?</h2>
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment people count"
+          onClick={() => onSetPeople(people + 1)
+          }
+        >
+          +
+        </button>
+        <button 
+          type="button"
+          className="counter__button"
+          aria-label="decrement people count"
+          onClick={() => onSetPeople(people - 1)}
+          
+        >
+          -
+        </button>
+      </div>
+    </>
+  );
+}
