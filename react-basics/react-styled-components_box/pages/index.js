c8b394514307c8d6e1255927fd605c57d6917bbb
Anna <awoellnitz@web.de>
2025-11-10 15:21:26 +0100

    box_finished

--- react-basics/react-styled-components_box/pages/index.js
+++ react-basics/react-styled-components_box/pages/index.js
@@ -1,10 +1,12 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
 
 export default function HomePage() {
   return (
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
     </div>
   );
 }
