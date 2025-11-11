e9300e1b11576513d3c1208274d70db6a64297cd
Anna <awoellnitz@web.de>
2025-11-11 08:05:03 +0100

    neworder_startedChallanges

---
+++ jsNEXT-basics/js-unit-testing_get-animal/index.js
@@ -0,0 +1,6 @@
+export function getAnimal(animal, animalPluralName) {
+    return "I" ${`totally love ${animal}!` ? `like ${animalPluralName}!` : `do not like animals at all!`}!
+}
+
+/* `I totally love ${animal}!` + getAnimals(animal)*/
+/* `I do not like animals at all!` + getAnimals()*/
