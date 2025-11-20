76f262cef732bc4768f04a1b8a99dff7ad92a8a7
Anna <awoellnitz@web.de>
2025-11-20 21:49:11 +0100

    chance_beginning

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,25 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
 // export const server = …
+export const server = createServer((request, response) => {
+    if (request.url === "/hello") {
+        response.statusCode = 200;
+        response.end("Hello Poeples");
+    } else {
+        response.statusCode = 404;
+        response.end("Error")
+    }
+}
+)
+const chance = new Chance(() => {
+    chance.name;
+    chance.age;
+    chance.profession;
+
+    /* 
+    `Hallo, mein Name ist ${name} 
+    und ich bin ${age} Jahre alt. 
+    Ich bin ${profession}.` 
+    */
+});
