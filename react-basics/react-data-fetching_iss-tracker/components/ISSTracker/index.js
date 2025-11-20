b3479ded00da1a5bc67a7d0f949a677fd302f9f7
Anna <awoellnitz@web.de>
2025-11-20 16:04:58 +0100

    react-basics_clean

---
+++ react-basics/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR, { SWRConfig } from "swr";
+
+
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+async function fetcher(url) {
+  const response = await fetcher(url);
+  const data = await response.json();
+  return data;
+}
+
+
+export default function ISSTracker() {
+  const {data, isLoading, error, mutate} = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+
+    if (error) {
+    return <h1>Error loading ISS data.</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+
+
+  return (
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+  /* const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  }); */
+
+ /*  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setCoords({ longitude: data.longitude, latitude: data.latitude });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  } */
+
+ /*  useEffect(() => {
+    const timer = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []); */
